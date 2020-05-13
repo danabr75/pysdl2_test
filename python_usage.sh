6896c9a42dcef08f8dc1c564ba10d91b70ff8c5a
Ben Dana <ben.dana@charity.org>
2020-05-12 21:34:59 -0400

    Initial commit

---
+++ python_usage.sh
@@ -0,0 +1,14 @@
+pyenv install 3.7.7
+pyenv global 3.7.7
+pyenv local 3.7.7
+
+brew install sdl2 sdl2_gfx sdl2_image sdl2_mixer sdl2_net sdl2_ttf
+
+pipenv shell
+
+pipenv install
+
+# To activate this project's virtualenv, run pipenv shell.
+
+# shell:
+pipenv install pygame
