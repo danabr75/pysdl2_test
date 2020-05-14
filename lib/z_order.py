86856399ed640f4ba5f91538d35a27cd27530182
Ben Dana <ben.dana@charity.org>
2020-05-13 23:11:44 -0400

    Added player class and factored out movement.

---
+++ lib/z_order.py
@@ -0,0 +1,1 @@
+Background, Building, Player, UI, Cursor = range(0, 5)
--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 # pipenv shell
 # python main.py
 
+
 # Simple pygame program
+
+# http://programarcadegames.com/index.php?chapter=example_code
+
 
 # Import and initialize the pygame library
 import pygame
 pygame.init()
+from lib import z_order
+from models.player import Player
+# print('test')
+# print(z_order.Background)
+# print(z_order.Building)
 
 # Set up the drawing window
+
 
 from pygame.locals import (
     K_UP,
@@ -27,6 +37,9 @@ print(K_w)
 print(pygame.K_w)
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+
+player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 block_pos_x = int(SCREEN_WIDTH/2)
@@ -42,59 +55,35 @@ move_down = False
 running = True
 while running:
     # Did the user click the window close button?
-
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         running = False
       elif event.type == KEYDOWN:
-        keys = pygame.key.get_pressed()
-        # print("keys pressed")
-        # print(keys)
-        # print("KEY DOWN")
-        # print(event.type)
-        # Was it the Escape key? If so, stop the loop.
         if event.key == K_ESCAPE:
           running = false
         if event.key == pygame.QUIT:
           running = false
-        if event.key == K_w:
-          move_up = true
-        if event.key == K_s:
-          move_down = true
-        if event.key == K_d:
-          move_right = true
-        if event.key == K_a:
-          move_left = true
+        else:
+          player.event_update(event)
       elif event.type == KEYUP:
-        if event.key == K_w:
-          move_up = false
-        if event.key == K_s:
-          move_down = false
-        if event.key == K_d:
-          move_right = false
-        if event.key == K_a:
-          move_left = false
-
-    if move_left:
-      block_pos_x -= 1
-    if move_right:
-      block_pos_x += 1
-    if move_up:
-      block_pos_y -= 1
-    if move_down:
-      block_pos_y += 1
+        player.event_update(event)
 
     # print("x,y:" + str(block_pos_x) + ' and ' + str(block_pos_y))
     # Fill the background with white
     screen.fill((255, 255, 255))
 
-    surf = pygame.Surface((50, 50))
-    surf.fill((0, 0, 0))
-    rect = surf.get_rect()
-    screen.blit(surf, (block_pos_x, block_pos_y))
+    # surf = pygame.Surface((50, 50))
+    # surf.fill((0, 0, 0))
+    # rect = surf.get_rect()
+
 
     # Draw a solid blue circle in the center
+    # pygame.draw.rect(screen, color, (x,y,width,height), thickness)
     pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    # screen.blit(surf, (block_pos_x, block_pos_y))
+    player.update()
+    player.draw(screen)
+    
 
     # Flip the display
     pygame.display.flip()
