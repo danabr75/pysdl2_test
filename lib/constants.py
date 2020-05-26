90e4cd5a5116aa9e98f62423385524ab14f9e611
Benjamin Dana <ben.dana@charity.org>
2020-05-26 01:10:55 -0400

    Update constants.py

--- lib/constants.py
+++ lib/constants.py
@@ -1,8 +1,22 @@
 import sdl2.ext
 
+from pathlib import Path
+import os
+
+
+
+# os.getcwd()
+# could replace __file__
+
+# in windows, this was still under lib, had to move assets under lib...
+data_folder = Path(__file__ + "/../assets")
+# print( Path(os.getcwd() + "/../assets") )
+
 WHITE = sdl2.ext.Color(255, 255, 255)
 GREEN = sdl2.ext.Color(0, 255, 0)
-RESOURCES = sdl2.ext.Resources(__file__, "../assets")
+# RESOURCES = sdl2.ext.Resources(__file__, "../assets" )
+# RESOURCES = sdl2.ext.Resources(__file__, "..\\assets"  Path(__file__).parents[0] )
+RESOURCES = sdl2.ext.Resources(str(data_folder))
 
 TILE_SIZE = 32
 SCREEN_WIDTH = 1024
@@ -10,6 +24,3 @@ SCREEN_HEIGHT = 768
 
 LIMIT_FPS = 60
 WINDOW_COLOR = (0, 0, 0, 255)
-
-
-
